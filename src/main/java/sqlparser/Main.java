4427a66b6c1a1f5d3039f6a6a5f0e8df0820c9e3
tinvukhac <tinvukhac@gmail.com>
2017-12-01 09:57:52 -0800

    add sample queries

--- src/main/java/sqlparser/Main.java
+++ src/main/java/sqlparser/Main.java
@@ -1,5 +1,6 @@
 package sqlparser;
 
+import java.io.File;
 import java.util.List;
 
 import org.apache.calcite.jdbc.CalciteConnection;
@@ -17,6 +18,7 @@ import org.apache.calcite.tools.Planner;
 import org.apache.calcite.tools.Programs;
 import org.apache.calcite.util.Holder;
 import org.apache.calcite.util.Util;
+import org.apache.commons.io.FileUtils;
 
 import com.google.common.base.Function;
 
@@ -24,16 +26,26 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println("Hello Apache Calcite");
-		Main test = new Main();
-		Planner planner = test.getPlanner();
-		String sql1 = "select * from customer where c_customer_id > 10";
-		String sql2 = "select i_item_desc ,i_category , i_current_price ,i_item_id ,sum(ws_ext_sales_price) as itemrevenue ,sum(ws_ext_sales_price)*100/sum(sum(ws_ext_sales_price)) as revenueratio from web_sales,item ,date_dim where web_sales.ws_item_sk = item.i_item_sk and item.i_category in ('Jewelry', 'Sports', 'Books') and web_sales.ws_sold_date_sk = date_dim.d_date_sk and date_dim.d_date between '2001-01-12' and '2001-02-11' group by i_item_id ,i_item_desc ,i_category ,i_current_price order by i_category ,i_item_id ,i_item_desc,revenueratio limit 100";
-		SqlNode parse = planner.parse(sql1);
-		System.out.println(parse.toString());
+		String filename = args[0];
+		String format = args[1];
+		
+		String query = FileUtils.readFileToString(new File(filename));
+		query = query.trim();
+		query = query.substring(0, query.length() - 1);
+		Main main = new Main();
+		Planner planner = main.getPlanner();
+		SqlNode parse = planner.parse(query);
 		SqlNode validate = planner.validate(parse);
 	    RelNode rel = planner.rel(validate).project();
-	    System.out.println(test.toString(rel));
+	    System.out.println(main.toString(rel, format));
+		
+//		String sql1 = "select * from customer where c_customer_id > 10";
+//		String sql2 = "select dt.d_year ,item.i_brand_id brand_id ,item.i_brand brand ,sum(ss_ext_sales_price) sum_agg from date_dim dt ,store_sales ,item where dt.d_date_sk = store_sales.ss_sold_date_sk and store_sales.ss_item_sk = item.i_item_sk and item.i_manufact_id = 436 and dt.d_moy=12 group by dt.d_year ,item.i_brand ,item.i_brand_id order by dt.d_year ,sum_agg desc ,brand_id limit 100";
+//		String sql3 = "select i_item_id, avg(ss_quantity) agg1, avg(ss_list_price) agg2, avg(ss_coupon_amt) agg3, avg(ss_sales_price) agg4 from store_sales, customer_demographics, date_dim, item, promotion where store_sales.ss_sold_date_sk = date_dim.d_date_sk and store_sales.ss_item_sk = item.i_item_sk and store_sales.ss_cdemo_sk = customer_demographics.cd_demo_sk and store_sales.ss_promo_sk = promotion.p_promo_sk and cd_gender = 'F' and cd_marital_status = 'W' and cd_education_status = 'Primary' and (p_channel_email = 'N' or p_channel_event = 'N') and d_year = 1998 group by i_item_id order by i_item_id limit 100";
+//		SqlNode parse = planner.parse(sql3);
+//		SqlNode validate = planner.validate(parse);
+//	    RelNode rel = planner.rel(validate).project();
+//	    System.out.println(test.toString(rel));
 	}
 
 	private Planner getPlanner() throws Exception {
@@ -62,9 +74,9 @@ public class Main {
 			+ "   schemas: [\n" + schema("TPCDS", "1.0") + ",\n" + schema("TPCDS_01", "0.01") + ",\n"
 			+ schema("TPCDS_5", "5.0") + "\n" + "   ]\n" + "}";
 	
-	public String toString(RelNode rel) {
+	public String toString(RelNode rel, String format) {
 	    return Util.toLinux(
-	        RelOptUtil.dumpPlan("", rel, SqlExplainFormat.JSON,
-	            SqlExplainLevel.NO_ATTRIBUTES));
+	        RelOptUtil.dumpPlan("", rel, format.equals("json")? SqlExplainFormat.JSON: SqlExplainFormat.TEXT,
+	            SqlExplainLevel.ALL_ATTRIBUTES));
 	  }
 }
